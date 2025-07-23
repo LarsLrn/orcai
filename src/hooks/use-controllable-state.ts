997376abd2bdbab29d74c2782bd769ac451b95ff
LarsLrn <rotambus@googlemail.com>
2025-07-23 11:27:10 +0200

    refactor: remove default value for `onChange` in `useControllableState`
    * Updated the `useControllableState` hook to remove the default
    empty function for the `onChange` parameter.

--- src/hooks/use-controllable-state.ts
+++ src/hooks/use-controllable-state.ts
@@ -16,7 +16,7 @@ type SetStateFn<T> = (prevState?: T) => T;
 function useControllableState<T>({
 	prop,
 	defaultProp,
-	onChange = () => {},
+	onChange,
 }: UseControllableStateParams<T>) {
 	const [uncontrolledProp, setUncontrolledProp] = useUncontrolledState({
 		defaultProp,
