600364c58a475f985deae6c397c26a9991d99f92
LarsLrn <rotambus@googlemail.com>
2025-07-23 22:49:25 +0200

    refactor(routes): add authentication redirection
    * Removed unused testing code.
    * Implemented `beforeLoad` to check user authentication and redirect accordingly.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,34 +1,15 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
-import { toast } from "sonner";
-import { Button } from "@/components/ui/button";
-import { db } from "@/db/drizzle";
-import { course } from "@/db/schema/course";
-
-const getCount = createServerFn({
-	method: "GET",
-}).handler(async () => {
-	const courses = await db
-		.select({ id: course.id, title: course.title })
-		.from(course);
-	return courses;
-});
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
 	component: Home,
-	loader: async () => await getCount(),
+	beforeLoad: ({ context }) => {
+		if (context.auth.isAuthenticated) {
+			return redirect({ to: "/app" });
+		}
+		return redirect({ to: "/login" });
+	},
 });
 
 function Home() {
-	const state = Route.useLoaderData();
-
-	return (
-		<div>
-			{state.map((course) => (
-				<div key={course.id}>{course.title}</div>
-			))}
-			<Button onClick={() => toast.success("Button clicked!")}>Test</Button>
-			<Button onClick={() => console.log("Button clicked")}>Test</Button>
-		</div>
-	);
+	return null;
 }
