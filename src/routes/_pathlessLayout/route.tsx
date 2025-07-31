7289508862e4f8f77473e5a5c0a2ac47ff2ef03e
LarsLrn <rotambus@googlemail.com>
2025-07-31 13:47:21 +0200

    fix: improve layout styling

--- src/routes/_pathlessLayout/route.tsx
+++ src/routes/_pathlessLayout/route.tsx
@@ -15,12 +15,10 @@ function PathlessLayoutComponent() {
 
 			<ThemeSwitcher className="fixed top-4 right-4 z-20 text-foreground" />
 
-			<div className="flex flex-grow items-center justify-center">
-				<div className="flex flex-col items-center justify-center gap-4 px-2 py-8">
-					<LogoTextAnimated variant="dynamic" className="h-20" />
+			<div className="flex flex-col items-center justify-center gap-4 px-2 py-8">
+				<LogoTextAnimated variant="dynamic" className="h-20" />
 
-					<Outlet />
-				</div>
+				<Outlet />
 			</div>
 
 			<div className="my-4 flex w-full justify-around p-2">
