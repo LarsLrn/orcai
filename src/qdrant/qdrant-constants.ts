6d93ca3cae6aa0dda73945562062ff61c812fa8b
LarsLrn <rotambus@googlemail.com>
2025-08-06 18:30:55 +0200

    chore: use more specific qdrant collection names

--- src/qdrant/qdrant-constants.ts
+++ src/qdrant/qdrant-constants.ts
@@ -2,8 +2,8 @@ export const qdrantCollections = {
 	asset: {
 		name:
 			process.env.NODE_ENV === "production"
-				? "sokratest-chunks-PROD"
-				: "sokratest-chunks-DEV",
+				? "sokratest-v2-chunks-PROD"
+				: "sokratest-v2-chunks-DEV",
 		dimensions: 4096,
 		index: {
 			repositoryId: "repository_id",
