5f74bf535fca3555de65cb3a68a99db1d5ed2e54
LarsLrn <rotambus@googlemail.com>
2025-09-30 13:16:56 +0200

    feat: update shadcn components

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,4 +1,5 @@
 import * as SeparatorPrimitive from "@radix-ui/react-separator";
+
 import { cn } from "@/lib/utils";
 
 function Separator({
@@ -9,7 +10,7 @@ function Separator({
 }: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
 	return (
 		<SeparatorPrimitive.Root
-			data-slot="separator-root"
+			data-slot="separator"
 			decorative={decorative}
 			orientation={orientation}
 			className={cn(
