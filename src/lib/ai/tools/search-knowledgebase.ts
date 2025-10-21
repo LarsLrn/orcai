66bd1be65ebab11aa9b2113fc6363cd30112dab8
LarsLrn <rotambus@googlemail.com>
2025-10-21 13:23:51 +0200

    feat(tools): improve RAG tool instructions

--- src/lib/ai/tools/search-knowledgebase.ts
+++ src/lib/ai/tools/search-knowledgebase.ts
@@ -5,33 +5,40 @@ import type { DatabaseBlock } from "@/lib/orpc/schemas/block";
 
 export const searchKnowledgeBaseTool = ({ block }: { block: DatabaseBlock }) =>
 	tool({
-		description: `ALWAYS base your response on the knowledge base. Use it to find relevant information about the user's query. If the results returned are not relevant, try to rephrase the query and search again. NEVER make up information that is not in the knowledge base. If you cannot find relevant information, respond with "I don't know".`,
+		description: `ALWAYS base your response on the knowledge base. Use it to find relevant information about the user's message. If the results returned are not relevant, try to rephrase the query and search again. NEVER make up information that is not in the knowledge base. If you cannot find relevant information, respond with "I don't know". Formulate long and detailed queries to get the best results. Expand abbreviations where possible.`,
 		inputSchema: z.object({
 			query: z
 				.string()
+				.describe("The query to search the knowledge base with."),
+			detailedQuery: z
+				.string()
 				.describe(
-					"The query to search the knowledge base with. If not directly specified, should be a summary of the conversation so far, focusing on the user's intent.",
+					"A more detailed version of the query with expanded abbreviations. Keep it precise yet detailed.",
 				),
-			limit: z
+			limit: z.coerce
 				.number()
 				.max(block.config.maxReferences)
 				.min(block.config.minReferences)
 				.describe("The number of results to return.")
 				.default(block.config.defaultReferences),
 		}),
-		execute: async ({ query, limit }) => {
+		execute: async ({ query, limit, detailedQuery }) => {
 			const result = await client.assetPoints.list({
 				filters: {
-					search: query,
+					search: detailedQuery,
 					limit: limit,
 					blockId: block.id,
 				},
 			});
 
-			const chunks = result.data.map((point) => ({
-				id: point.id,
-				text: point.payload.text,
-			}));
+			const chunks = result.data
+				.filter((point) => point.score > 0.5)
+				.map((point) => ({
+					id: point.id,
+					score: point.score,
+					title: point.payload.title,
+					text: point.payload.text,
+				}));
 
 			return {
 				result: chunks,
