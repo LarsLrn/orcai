eec7e23a28e5299843e8164d1833b2dc8867188e
LarsLrn <rotambus@googlemail.com>
2025-12-08 15:27:36 +0100

    feat(router): add serializer to query client
    * Improved data handling for queries to prevent immediate refetching on mount.

--- src/router.tsx
+++ src/router.tsx
@@ -1,3 +1,4 @@
+import { StandardRPCJsonSerializer } from "@orpc/client/standard";
 import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
@@ -7,7 +8,31 @@ import { deLocalizeUrl, localizeUrl } from "./paraglide/runtime";
 import { routeTree } from "./routeTree.gen";
 
 export function getRouter() {
-	const queryClient = new QueryClient();
+	const serializer = new StandardRPCJsonSerializer();
+
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				queryKeyHashFn(queryKey) {
+					const [json, meta] = serializer.serialize(queryKey);
+					return JSON.stringify({ json, meta });
+				},
+				staleTime: 60 * 1000, // > 0 to prevent immediate refetching on mount
+			},
+			dehydrate: {
+				serializeData(data) {
+					const [json, meta] = serializer.serialize(data);
+					return { json, meta };
+				},
+			},
+			hydrate: {
+				deserializeData(data) {
+					return serializer.deserialize(data.json, data.meta);
+				},
+			},
+		},
+	});
+
 	const router = createRouter({
 		routeTree,
 		rewrite: {
