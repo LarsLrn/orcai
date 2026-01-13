46ed525a4f4b3433cc7de7568244b4855896a484
LarsLrn <rotambus@googlemail.com>
2026-01-14 00:20:31 +0100

    feat: drop language prefix from english path

--- vite.config.ts
+++ vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
 				{
 					pattern: "/:path(.*)?",
 					localized: [
-						["en", "/en/:path(.*)?"],
+						["en", "/:path(.*)?"],
 						["de", "/de/:path(.*)?"],
 					],
 				},
