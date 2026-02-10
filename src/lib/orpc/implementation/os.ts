f2ed00cf5959f183d28025b1f9ab5028d4439ad0
LarsLrn <rotambus@googlemail.com>
2026-02-10 16:35:00 +0100

    refactor: remove Pino from ORPC context
    * All ORPC handlers will eventually be handled by Effect, which already integrates with Pino.

--- src/lib/orpc/implementation/os.ts
+++ src/lib/orpc/implementation/os.ts
@@ -1,9 +1,8 @@
-import type { LoggerContext } from "@orpc/experimental-pino";
 import { implement } from "@orpc/server";
 import type { RequestHeadersPluginContext } from "@orpc/server/plugins";
 import { contracts } from "@/lib/orpc/contracts";
 
-interface ORPCContext extends LoggerContext, RequestHeadersPluginContext {
+interface ORPCContext extends RequestHeadersPluginContext {
 	meta?: { zedToken?: string };
 }
 
