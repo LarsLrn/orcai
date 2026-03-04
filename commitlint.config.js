9be1dd09edda0907d743366557a6ec0d5f6dfb9e
LarsLrn <rotambus@googlemail.com>
2026-03-04 17:38:32 +0100

    ci: ignore merge and revert in commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,10 @@ export default {
 	extends: [
 		"@commitlint/config-conventional",
 	],
+	ignores: [
+		(message) => message.startsWith("Merge "),
+		(message) => message.startsWith("Revert "),
+	],
 	rules: {
 		"body-max-line-length": [
 			0,
