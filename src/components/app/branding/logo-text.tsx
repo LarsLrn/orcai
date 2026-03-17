0e2d1a60a835e1fd8d21d01e13048f76bc4cc2ac
LarsLrn <rotambus@googlemail.com>
2026-03-17 12:51:22 +0100

    refactor: move legal and branding assets into src
    * This enables bundling with vite and generated file name hashes, which allows cache busting on static asset changes.

--- src/components/app/branding/logo-text.tsx
+++ src/components/app/branding/logo-text.tsx
@@ -1,17 +1,19 @@
 import { cn } from "@/lib/utils";
+import textColorLogo from "@/static/branding/text_color.svg";
+import textWhiteLogo from "@/static/branding/text_white.svg";
 
 const LogoText = ({ className }: { className?: string }) => {
 	return (
 		<div className={cn("flex w-full items-center justify-center", className)}>
 			<img
-				src="/logo/text_color.svg"
+				src={textColorLogo}
 				alt="OrcAI logo"
 				width={150}
 				height={40}
 				className="h-14 w-auto dark:hidden"
 			/>
 			<img
-				src="/logo/text_white.svg"
+				src={textWhiteLogo}
 				alt="OrcAI logo"
 				width={150}
 				height={40}
--- src/routes/_pathlessLayout/privacy.tsx
+++ src/routes/_pathlessLayout/privacy.tsx
@@ -1,33 +1,13 @@
-import fs from "node:fs";
-import path from "node:path";
 import { createFileRoute } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
 import { Markdown } from "@/components/app/markdown";
 import { Card, CardContent } from "@/components/ui/card";
-
-const getMarkdownContent = createServerFn({
-	method: "GET",
-}).handler(() => {
-	const markdownPath = path.join(process.cwd(), "src/md/privacy-policy.md");
-	const markdownContent = fs.readFileSync(markdownPath, "utf8");
-
-	return markdownContent;
-});
+import markdownContent from "@/static/legal/privacy-policy.md?raw";
 
 export const Route = createFileRoute("/_pathlessLayout/privacy")({
 	component: RouteComponent,
-	loader: async () => {
-		const content = await getMarkdownContent();
-		return {
-			content,
-		};
-	},
-	ssr: "data-only",
 });
 
 function RouteComponent() {
-	const { content: markdownContent } = Route.useLoaderData();
-
 	return (
 		<Card className="container mx-auto py-8">
 			<CardContent className="mx-auto max-w-4xl">
